05423df00446e2185d4c60117361627bc8affe65
kadolfo9 <kaykyadolfo21@hotmail.com>
2026-01-04 10:22:15 -0300

    refactor: using controller routes

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,11 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [DashboardController::class, 'index'])->name('profile');
 });
 
 Route::controller(AuthController::class)->group(function () {
@@ -18,4 +15,21 @@ Route::controller(AuthController::class)->group(function () {
 
     Route::get('/register', 'registerIndex')->name('register');
     Route::post('/register', 'register')->name('register.request');
+});
+
+Route::controller(PostController::class)
+    ->middleware('auth')
+    ->group(function () {
+    Route::get('/posts', 'index')->name('posts');
+    Route::post('/posts', 'store')->name('posts.store');
+
+    Route::get('/posts/{post}', 'show')->name('posts.show');
+    Route::put('/posts/{post}', 'update')->name('posts.update');
+    Route::delete('/posts/{post}', 'destroy')->name('posts.destroy');
+});
+
+Route::controller(DashboardController::class)
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('/profile', 'index')->name('profile');
 });
